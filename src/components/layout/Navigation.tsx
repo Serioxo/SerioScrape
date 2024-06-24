f6c00f707c9aaf0222c715f20628c9b66741f04d
Serio <jakubmigac16@gmail.com>
2024-06-24 02:39:42 +0100

    changes

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -92,23 +92,6 @@ export function Navigation(props: NavigationProps) {
                 onClick={() => window.scrollTo(0, 0)}
               >
                 <BrandPill clickable header />
-              </Link>
-              <a
-                href={conf().DISCORD_LINK}
-                target="_blank"
-                rel="noreferrer"
-                className="text-xl text-white tabbable rounded-full"
-              >
-                <IconPatch icon={Icons.DISCORD} clickable downsized />
-              </a>
-              <a
-                href={conf().GITHUB_LINK}
-                target="_blank"
-                rel="noreferrer"
-                className="text-xl text-white tabbable rounded-full"
-              >
-                <IconPatch icon={Icons.GITHUB} clickable downsized />
-              </a>
               <a
                 onClick={() => handleClick("/discover")}
                 rel="noreferrer"
